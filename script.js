9bb44a4928b625ba8a182143395bc23cffb2f1d2
CarylThom <cat.twnti@gmail.com>
2022-04-15 10:44:48 +0000

    Add query selectors to script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
+let playerSequence = [];
+let computerSequence = [];
+let level = 0;
 
-function startGame(){
+/*const*/
+const gameHeading = document.querySelector('.javascript-heading');
 
-}
+const buttonContainer = document.querySelector('.game-container');
+
+const startButton = document.querySelector('.javascript-start');
+
+const info = document.querySelector('.javascript-progress');
+
+
+
+  
+
+
+
 
 function playerTurn(){
 
